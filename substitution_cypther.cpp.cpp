034c111b27981b8594b42df6addc2388ef8023d6
HazemShoaib <145262650+HazemShoaib@users.noreply.github.com>
2024-03-10 22:45:30 +0200

    substitution_cypther.cpp.cpp

--- substitution_cypther.cpp.cpp
+++ substitution_cypther.cpp.cpp
@@ -1,4 +1,4 @@
-// FCAI – Structured Programming – 2024 - Assignment 2
+// FCAI â€“ Structured Programming â€“ 2024 - Assignment 2
 // Program Name: substitution_cypther.cpp
 // Program Description: this code is a simple substitution cipher which works by placing a
 //                      "key" in the beginning of a new alphabet then placing the rest of the letters
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <cctype>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,33 +18,43 @@ char alpha[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 char encrypt_txt[50], decrypt_txt[50];
 string letters = "";
 
-string get_letters(){       //this function is used to make a new alphabet to encrypt with
+string get_letters(){       //this function is used to make a new alphabet to substitute the letters with
 
     string  key = "";
     bool flag = true;
+    int occurrence;
 
     while (
-    (flag)
-    || (key.size()>5)
-    || (key.size()==0)){
-
+            (flag)
+            || (key.size()>5)
+            || (key == "\n")
+            || occurrence >1){
         cout << "please enter your key from 5 letters with no repetition or special characters (',' , ';' , ''' , '\' ....etc):" << endl;
         cin >> key;
+
         for(int i=0; i<key.size(); i++){
             key[i] = tolower(key[i]);
-            if (!isalpha(key[i])){      //it checks the the key meets the conditions to be valid
+            if (!isalpha(key[i])){          //here it checks if the the key meets the conditions in order to be valid
                 flag = true;
                 break;
             }
+
             else {
                 flag = false;
             }
         }
+        for(char ch:key){                   //here it checks if the the key has any reoccurring letters
+            occurrence = count(key.begin(),key.end(), ch);
+            if(occurrence>1)
+                break;
+        }
+
     }
+
     flag = false;
     letters += key;                     //it puts the key in the beginning of the alphabet
-    for (int i=0; i<26; i++){
-        for (int r=0; r<letters.size(); r++){       //then puts the remaining letters
+    for (int i=0; i<26; i++){           //then puts the remaining letters without repetition
+        for (int r=0; r<letters.size(); r++){
             if (alpha[i] == letters[r]){
                 flag = false;
                 break;
@@ -56,25 +67,24 @@ string get_letters(){       //this function is used to make a new alphabet to en
         if (flag == true){
             letters += alpha[i];
             flag = false;
-            }
+        }
     }
-
 
     return letters;
 }
 
-string encrypt(){
+
+string encryption_simple(){
 
     string text = "";
     int n = 0;
-
     get_letters();
-
-    cout << "please enter the text you want to encrypt:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, text);
-
-    for (int r=0; r<text.size(); r++){      //it replaces every letter with its equivalent in the new alphabet
+    while(text.empty()){
+        cout << "please enter the text you want to encrypt:" << endl;
+        cin.ignore(1, '\n');
+        getline(cin, text);
+    }
+    for (int r=0; r<text.size(); r++){                          //it replaces every letter with its equivalent in the new alphabet
         text[r] = tolower(text[r]);
         for (int i=0; i<26; i++){
             if (text[r] == alpha[i]){
@@ -96,18 +106,17 @@ string encrypt(){
     return encrypt_txt;
 }
 
-string decrypt(){
+string decryption_simple(){
 
     string text = "";
     int n = 0;
-
     get_letters();
-
-    cout << "please enter the text you want to decrypt:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, text);
-
-    for (int r=0; r<text.size(); r++){                  //it replaces every letter with its equivalent in the normal alphabet
+    while(text.empty()){
+        cout << "please enter the text you want to decrypt:" << endl;
+        cin.ignore(1, '\n');
+        getline(cin, text);
+    }
+    for (int r=0; r<text.size(); r++){                      //it replaces every letter with its equivalent in the normal alphabet
         text[r] = tolower(text[r]);
         for (int i=0; i<26; i++){
             if (text[r] == letters[i]){
@@ -140,10 +149,10 @@ int main(){
         cin >> choice;
 
         if (choice == 1){
-            encrypt();
+            encryption_simple();
         }
         else if (choice == 2){
-            decrypt();
+            decryption_simple();
         }
     }
     return 0;
